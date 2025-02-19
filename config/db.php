274464bcd7398a9ae32ed668b9df139d7194f19b
Bryan Emmanuel <159569362+BryanEmmanueldev99@users.noreply.github.com>
2025-02-18 18:14:56 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=wc_store_erp',
+    'dsn' => 'mysql:host=localhost;dbname=?',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
